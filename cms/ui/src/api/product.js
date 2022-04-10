93a69c937d1b00e7f667a3922b1ade191d5708e6
asalehi <engsalehi@live.com>
2022-04-11 00:20:41 +0430

    add Pagination Component

--- cms/ui/src/api/product.js
+++ cms/ui/src/api/product.js
@@ -1,12 +1,20 @@
 const API_BASE_URL = 'http://localhost:3080/product/all';
 const productApi = {
-    loadProduct : async () => {
+    loadProduct : async (prodId) => {
         let loaded = [];
         await fetch(API_BASE_URL)
             .then(data => data.json())
             .then(data => loaded = data);
 
-        return loaded
+        if(prodId) {
+            return loaded.filter((item) => {
+                return Number(item.productId) === Number(prodId);
+            })
+        }else {
+            return loaded;
+        }
+
+
     }
 
 }
--- cms/ui/src/pages/cardex/productCardex.jsx
+++ cms/ui/src/pages/cardex/productCardex.jsx
@@ -5,17 +5,31 @@ import productApi from '../../api/product';
 // Define States
 
 
-const load = () => {
-    console.log(productApi.loadProduct());
+
+const PicturesCard = ({pictures}) =>{
+    return(
+        <React.Fragment>
+            {pictures[0].title}
+        </React.Fragment>
+    );
 }
 
 const ProductProfile = ({formHeader, formType}) => {
 
     const [products, setProducts] = useState([]);
-    const [filter, setFilter] = useState('');
+    const [filter, setFilter] = useState(0);
 
     const filterProduct = (e) => {
         setFilter(e.target.value);
+    }
+
+    const load = () => {
+
+        productApi.loadProduct(filter)
+            .then(data => {
+                setProducts(data);
+            });
+
     }
 
     return (
@@ -26,15 +40,22 @@ const ProductProfile = ({formHeader, formType}) => {
                     <div className="row">
                         <div className="col-lg-2">`
                             <div className="form-group">
-                                <label>کد کالا</label>
+
                                 <input type="text"
-                                       data-parsley-type="string"
+                                       data-parsley-type="number"
                                        id="txtSearch"
                                        className="form-control"
                                        required
                                        onChange={filterProduct}
                                        placeholder="کد کالا"/>
                             </div>
+                        </div>
+                        <div className="col-lg-2 mt-3">
+                            <button type="button" className="btn btn-success waves-effect waves-light"
+                                    data-toggle="modal"
+                                    onClick={load}
+                                    data-target=".bs-example-modal-center">تامین کننده جدید
+                            </button>
                         </div>
                     </div>
                 </div>
@@ -47,6 +68,7 @@ const ProductProfile = ({formHeader, formType}) => {
                             <div className="card m-b-30">
                                 <div className="card-body">
                                     <h4 className="mt-0 header-title">عکس های مربوطه به کالا</h4>
+                                    <PicturesCard pictures={products}/>
                                 </div>
                             </div>
                         </div>
