bf39f51c977bc1f898d7b6c1f3231a2435e25722
alimoshe <engsalehi@live.com>
2022-04-12 12:40:47 +0430

    Add product-profile page

--- cms/ui/src/pages/cardex/productCardex.jsx
+++ cms/ui/src/pages/cardex/productCardex.jsx
@@ -5,79 +5,16 @@ import productApi from '../../api/product';
 
 // Define States
 
-const PicturesCard =  ({products, filter, addImage}) => {
-    const [prodImage, setProdImage] = useState([]);
+const PicturesCard = ({imageName}) => {
+    return(
+        <img className="img-thumbnail"
+             alt="200x200"
+             style={{width: '200px', height: '200px'}}
+             src={imageName} data-holder-rendered="true"/>
+    )
 
-
-    if (Number(filter) > 0) {
-        const filtered = products.filter((item) => {
-            return Number(item.productId) === Number(filter);
-        });
-
-
-        if (filtered.length === 1) {
-            const fetchImg = async () => {
-                let images = [];
-                filtered[0].pictures.map(async (data, index)=> {
-                    const res = await productApi.loadImageFile(filter, index);
-                    const imgBLOB = await res.blob();
-                    const imageObjectUrl = URL.createObjectURL(imgBLOB);
-                    images.push(imageObjectUrl);
-                    addImage(imageObjectUrl);
-
-                })
-                return images;
-            }
-
-
-
-
-            // var imagesTag = [];
-            // fetchImg().then(sample => {
-            //
-            //     sample.map((data,index) => {
-            //         console.log(data);
-            //         imagesTag.push(`<img class="img-thumbnail"
-            //          alt="200x200"
-            //          style=${{width: '200px', height: '200px'}}
-            //          src=${data} data-holder-rendered="true" />`);
-            //
-            //     })
-            //     console.log(imagesTag);
-            // });
-
-            return(
-
-            <React.Fragment>
-                <PicPane Pics={[]}/>
-            </React.Fragment>
-            )
-            /*
-            filtered[0].pictures.map((img) => {
-                productApi.loadImagesPath(img).then((record) => {
-                    path.push(record.data.path);
-                })
-            });*/
-        }
-    }
-return <></>
 }
 
-const PicPane = ({Pics}) => {
-
-return(
-    <div>
-        {
-            Pics.map(img => (
-                <img className="img-thumbnail"
-                     alt="200x200"
-                     style={{width: '200px', height: '200px'}}
-                     src={img} data-holder-rendered="true" />
-            ))
-        }
-    </div>
-)
-}
 
 
 
@@ -85,21 +22,51 @@ const ProductProfile = ({formHeader, formType}) => {
 
     const [products, setProducts] = useState([]);
     const [filter, setFilter] = useState(0);
-
+    const [singleImage, setSingleImage] = useState('');
+    const [images, setImages] = useState([]);
     const load = (e) => {
         e.preventDefault();
         productApi.loadProduct()
             .then(data => {
                 setProducts(data);
+                if (Number(filter) > 0)
+                {
+
+                    const filtered = products.filter((item) => {
+                        return Number(item.productId) === Number(filter);
+                    });
+
+                    if (filtered.length === 1) {
+                        const fetchImg = async () => {
+                            let convertedImages = [];
+
+                            filtered[0].pictures.map(async (data, index) => {
+                                const res = await productApi.loadImageFile(filter, index);
+                                const imgBLOB = await res.blob();
+                                const imageObjectUrl = URL.createObjectURL(imgBLOB);
+                                convertedImages.push(imageObjectUrl);
+                            },this)
+                            //console.log(convertedImages);
+                            setImages(convertedImages);
+
+                        }
+                        fetchImg().then();
+                    }
+                }
+
+
             });
+        console.log(images);
+
     }
 
     const setFilterProduct = (e) => {
         setFilter(Number(e.target.value));
     }
 
-    const addProdImage = (imageName) =>{
-
+    const addProdImage = (imageName) => {
+       // setSingleImage(imageName);
+        //console.log(imageName);
     }
 
     return (
@@ -138,10 +105,12 @@ const ProductProfile = ({formHeader, formType}) => {
                             <div className="card m-b-30">
                                 <div className="card-body">
                                     <h4 className="mt-0 header-title">عکس های مربوطه به کالا</h4>
-                                    <PicturesCard products={products || []}
-                                                  filter={filter}
-                                                  addImage={addProdImage}
-                                    />
+                                    {
+                                        images.map((data, index)=>(
+                                            <PicturesCard key={index} imageName={data} />
+                                        ))
+                                    }
+
                                 </div>
                             </div>
                         </div>
